fe800091558b1cea741e57d88e374d42c8e0a495
Mush-Man <mubangamushili.p@gmail.com>
2024-11-29 12:20:38 +0200

    Update System.py

--- Views/System.py
+++ Views/System.py
@@ -14,8 +14,8 @@ from datetime import datetime
 @st.cache_resource
 def load_yolo_models():
     try:
-        model_1 = YOLO("https://github.com/Mush-Man/Streamlit_WebApp_demo/blob/main/best.pt")
-        model_2 = YOLO("https://github.com/Mush-Man/Streamlit_WebApp_demo/blob/main/best%20(1).pt")
+        model_1 = YOLO("https://drive.google.com/file/d/1MMNzrExJRw14OZTmcaMRk6dfuCfzsPwi/view?usp=sharing")
+        model_2 = YOLO("https://drive.google.com/file/d/1-5TdFXe7D4t4ewIqc3lTfFFbKXNn-nlV/view?usp=sharing")
         return model_1, model_2
     except Exception as e:
         st.error(f"Error loading YOLO models: {e}")
